615b6631275b705f0c0929f34dd6538a5f0235af
ZhangLei <lei977@126.com>
2019-08-04 08:13:25 +0800

    模块修改

---
+++ cloud-service-web/src/main/java/com/lei977/web/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.lei977.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(DemoApplication.class);
+	}
+
+}
---
+++ cloud-service-web/src/main/java/com/lei977/web/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.lei977.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello, Zbook!";
+	}
+}
